8981f3c00a6065aa69686778c826e2d332825102
Aaron Nance <apnance@hotmail.com>
2023-05-05 17:23:15 -0700

    Add

---
+++ iLibertas/Configs.swift
@@ -0,0 +1,23 @@
+//
+//  Configs.swift
+//  iLibertas
+//
+//  Created by Aaron Nance on 6/9/22.
+//
+
+import Foundation
+
+struct Configs {
+    
+    struct History {
+        
+        struct File {
+            
+            static let name = "history"
+            static let subdir = ""
+            
+        }
+        
+    }
+    
+}
